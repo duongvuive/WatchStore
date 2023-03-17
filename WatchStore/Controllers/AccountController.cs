8da51fd178e4bf6cc036bb409560b06a4bed57bb
Hoài Dương Võ <vohoaiduong2015@gmail.com>
2023-03-17 16:41:35 +0700

    mới cập nhật thêm tính năng đăng ký thông tin tài khoản

--- WatchStore/Controllers/AccountController.cs
+++ WatchStore/Controllers/AccountController.cs
@@ -156,14 +156,13 @@ namespace WatchStore.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
+                    Session["Email"] = model.Email;
+                    return RedirectToAction("DangKyThongTin", "KhachHang");
                 }
                 AddErrors(result);
             }
@@ -171,6 +170,9 @@ namespace WatchStore.Controllers
             // If we got this far, something failed, redisplay form
             return View(model);
         }
+        #region dang ky nhan vien
+        
+        #endregion
 
         //
         // GET: /Account/ConfirmEmail
---
+++ WatchStore/Controllers/KhachHangController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatchStore.Models;
+
+namespace WatchStore.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        dbDongHoDataContext data = new dbDongHoDataContext("Data Source=DESKTOP-NEIOBVT;Initial Catalog=WatchStore;Integrated Security=True");
+        public ActionResult DangKyThongTin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKyThongTin(FormCollection collection, Customer kh)
+        {
+            string email = Session["Email"].ToString();
+            var GetId = data.AspNetUsers.First(m=>m.Email == email);
+            var IDCustomer = from b in data.AUTO_IDCustomer() select b;
+            var FullName = collection["FullName"];
+            var Gender = collection["Gender"];
+            var CitizenIdentification = collection["CitizenIdentification"];
+            var Address = collection["Address"];
+            
+
+            foreach (var item in IDCustomer)
+            {
+                kh.IDCustomer += item;
+            }
+
+            kh.FullName = FullName;
+            kh.Gender = Gender;
+            kh.CitizenIdentification = CitizenIdentification;
+            kh.Address = Address;
+            kh.Id = GetId.Id.ToString();
+            kh.IDAP = 1;
+            data.Customers.InsertOnSubmit(kh);
+            data.SubmitChanges();
+            return RedirectToAction("Index","Watch");
+
+            /*   return this.DangKyThongTin();*/
+
+        }
+    }
+}
