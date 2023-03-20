56b9dea1fe643dfe2b212922c5a628789dfe5bd8
Tran Minh <kgtminh@gmail.com>
2023-03-20 20:53:06 +0700

    Giỏ Hàng

---
+++ WatchStore/Controllers/GioHangController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatchStore.Models;
+
+namespace WatchStore.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        dbDongHoDataContext db = new dbDongHoDataContext("Data Source=FREEDY\\SQLEXPRESS;Initial Catalog=hi;Integrated Security=True");
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+        public ActionResult ThemGioHang(int id, string strURL)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang watch = lstGioHang.Find(n => n.IDWatch == id);
+            if (watch == null)
+            {
+                watch = new GioHang(id);
+                lstGioHang.Add(watch);
+                return Redirect(strURL);
+            }
+            else
+            {
+                watch.iSoLuong++;
+                return Redirect(strURL);
+            }
+        }
+        private int TongSoLuong()
+        {
+            int tsl = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                tsl = lstGioHang.Sum(n => n.iSoLuong++);
+            }
+            return tsl;
+        }
+        private int TongSoLuongSanPham()
+        {
+            int tsl = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                tsl = lstGioHang.Count;
+            }
+            return tsl;
+        }
+        private double TongTien()
+        {
+            double tt = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                tt = lstGioHang.Sum(n => n.ThanhTien);
+            }
+            return tt;
+        }
+        public ActionResult GioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return View(lstGioHang);
+        }
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoLuongSanPham();
+            return PartialView();
+        }
+        public ActionResult XoaGioHang(int id)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.IDWatch == id);
+            if (sanpham != null)
+            {
+                lstGioHang.RemoveAll(n => n.IDWatch == id);
+                return RedirectToAction("GioHang");
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult CapNhatGioHang(int id, FormCollection collection)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.IDWatch == id);
+            if (sanpham != null)
+            {
+                sanpham.iSoLuong = int.Parse(collection["txtSolg"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            lstGioHang.Clear();
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WatchStore/Models/GioHang.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace WatchStore.Models
+{
+    [Serializable]
+    public class GioHang
+    {
+        dbDongHoDataContext db = new dbDongHoDataContext("Data Source=FREEDY\\SQLEXPRESS;Initial Catalog=hi;Integrated Security=True");
+        private int id;
+
+        public int IDWatch { get; set; }
+        [Display(Name = "Mã Đồng hồ")]
+        public string NameWatch { get; set; }
+        [Display(Name = "Tên Đông Hồ")]
+        public string Image { get; set; }
+        [Display(Name = "Hình")]
+        
+        public int iSoLuong { get; set; }
+        [Display(Name = "So Luong")]
+        public Double Price { get; set; }
+        [Display(Name = "Đơn Giá")]
+        public Double ThanhTien
+        {
+            get { return Price; }
+        }
+        
+        
+        public GioHang(string IDWatch )
+        {
+            
+            Watch watch = db.Watches.Single(n => n.IDWatch == IDWatch);
+            NameWatch = watch.NameWatch;
+            Image = watch.Image;
+            Price = double.Parse(watch.Price.ToString());
+        }
+
+        public GioHang(int id)
+        {
+            this.id = id;
+        }
+    }
+}
